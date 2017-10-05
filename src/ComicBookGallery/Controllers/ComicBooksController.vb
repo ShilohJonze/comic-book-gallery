aff2a364cea81ad1a720577406f76f4546154180
Shiloh Mount <Shilohjonze@gmail.com>
2017-10-05 17:12:18 -0400

    Added ComicBookController

---
+++ src/ComicBookGallery/Controllers/ComicBooksController.vb
@@ -0,0 +1,14 @@
+﻿Using System;
+	Using System.Collections.Generic;
+	Using System.Linq;
+	Using System.Web;
+	Using System.Web.Mvc;
+
+
+Namespace ComicBookGallery.Controllers
+{
+	Public Class ComicBooksController :  Controller
+	{
+	}
+
+	}
